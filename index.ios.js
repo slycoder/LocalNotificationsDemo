cd6b994a9f40c0c0b84d5ec6ed36f3028ae760bd
slycoder <jonathan.chang@zipongo.com>
2015-07-08 20:49:06 -0700

    Actually make the demo app do something

--- index.ios.js
+++ index.ios.js
@@ -7,25 +7,60 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  PushNotificationIOS,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } = React;
 
 var LocalNotificationsDemo = React.createClass({
+  getInitialState: function() {
+    return {
+      message: 'Welcome!',
+    };
+  },
+  componentWillMount: function() {
+    PushNotificationIOS.addEventListener('local_notification', this._onNotification);
+  },
+  componentWillUnmount: function() {
+    PushNotificationIOS.removeEventListener('local_notification', this._onNotification);
+  },
+  _onNotification: function(notification) {
+    this.setState({
+      message: notification.getData().message,
+    });
+  },
+  doPress: function(newLabel) {
+    var notificationTime = new Date();
+    // 10 seconds later...
+    notificationTime.setTime(notificationTime.getTime() + 10 * 1000);
+    PushNotificationIOS.scheduleLocalNotification({
+      fireDate: notificationTime,
+      alertBody: 'Test: ' + newLabel,
+      userInfo: {
+        message: newLabel,
+      },
+    });
+  },
   render: function() {
+    var buttons = [
+      'foo',
+      'bar',
+      'baz',
+    ];
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {this.state.message}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        {buttons.map((button) => (
+          <TouchableHighlight style={styles.buttonContainer} onPress={() => this.doPress(button)}>
+            <Text style={styles.button} key={button}>
+              {button}
+            </Text>
+          </TouchableHighlight>
+        ))}
       </View>
     );
   }
@@ -43,10 +78,15 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  buttonContainer: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 10,
+  },
+  button: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: 'white',
+    fontSize: 20,
   },
 });
 
